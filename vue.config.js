2cd598564c7cc9929769016882bfacdd59ab7392
zxy1764572240 <1764572240@qq.com>
2022-06-11 03:13:50 +0800

    初始化

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'pages': '@/pages',
+        'components': '@/components',
+        'network': '@/network',
+
+      }
+    }
+  }
+}
